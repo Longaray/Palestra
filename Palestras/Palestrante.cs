915d4d057e67af2b5447be83940ead8b127caa17
Longaray <rodrigolongaray@gmail.com>
2017-03-17 12:48:08 -0300

    Initial Files - Working

---
+++ Palestras/Palestrante.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Palestras
+{
+    class Palestrante
+    {
+        private String name;
+        private int time;
+        private bool alocation;
+
+        public Palestrante(String name, int time) {
+            this.name = name;
+            this.time = time;
+            this.alocation = false;
+        }
+
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+
+        public void setTime(int time)
+        {
+            this.time = time;
+        }
+
+        public void setAllocation(bool alocation)
+        {
+            this.alocation = alocation;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public int getTime()
+        {
+            return time;
+        }
+
+        public bool getAlocation()
+        {
+            return alocation;
+        }
+
+        public string toString()
+        {
+            return "Palestrante: "+name+" Tempo: "+time+" minutos.";
+        }
+    }
+}
---
+++ Palestras/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Palestras
+{
+    class Program
+    {
+        //List for palestrante
+        private static List<Palestrante> listPalestrante;
+
+        static void Main(string[] args)
+        {
+            //List for palestrante
+            listPalestrante = new List<Palestrante>();
+
+            //Basic code taken from MSDN
+            // Read each line of the file into a string array. Each element
+            // of the array is one line of the file.
+            string[] lines = System.IO.File.ReadAllLines(@"entrada.txt");
+
+            // Display the file contents by using a foreach loop.
+            //System.Console.WriteLine("Contents of WriteLines2.txt = ");
+            foreach (string line in lines)
+            {
+                // Use a tab to indent each line of the file.
+                //Console.WriteLine("\t" + line);
+                string[] splitLine = line.Split(';');
+                listPalestrante.Add(new Palestrante(splitLine[0], Convert.ToInt16(splitLine[1])));
+
+            }
+
+            //DEBUG: eliminate final version
+            /*foreach(Palestrante aux in listPalestrante)
+            {
+                Console.WriteLine(aux.toString());
+            }*/
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+}
